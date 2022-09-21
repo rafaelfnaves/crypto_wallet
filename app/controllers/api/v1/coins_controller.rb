87787fe806098b9937b88fe755fd0c43d6635ef1
Rafael Naves <r.naves21@gmail.com>
2022-09-20 22:03:20 -0300

    Change find coin show

--- app/controllers/api/v1/coins_controller.rb
+++ app/controllers/api/v1/coins_controller.rb
@@ -20,7 +20,7 @@ class Api::V1::CoinsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
-      @coin = Coin.find(params[:id])
+      @coin = Coin.find_by_asset_id(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.

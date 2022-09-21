9cdb92ffb4b4df078fe1f075dd6be17f04ea9672
Rafael Naves <r.naves21@gmail.com>
2022-09-20 21:55:07 -0300

    Changed json_web_token

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -1,11 +1,13 @@
 class AuthenticateUser
   prepend SimpleCommand
 
+  #this is where parameters are taken when the command is called
   def initialize(email, password)
     @email = email
     @password = password
   end
 
+  #this is where the result gets returned
   def call
     JsonWebToken.encode(user_id: user.id) if user
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# User.create!(email: 'gustavolzss12@gmail.com' , password: 'EihdF6P0y5' , password_confirmation: 'EihdF6P0y5')
